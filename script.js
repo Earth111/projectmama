d06e91797d656fbf1fb3c1ebe946d5ad46fb4c52
Earth111 <eikoc5758@gmail.com>
2026-02-02 23:58:40 +0700

    แก้

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ addEventOnElem(navbarLinks, "click" , closeNavbar);
 const header = document.querySelector("[data-header]");
 
 const headerActive = function () {
-    if(window.screenY > 150) {
+    if(window.scrollY > 350) {
         header.classList.add("active");
     } else {
         header.classList.remove("active");
